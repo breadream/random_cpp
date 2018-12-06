1a3f2fe4cc2ee6b1d057b4c6ef54d3082ce168cb
breadream <brad.dh.lim@gmail.com>
2018-12-06 17:21:59 -0600

    Added another version to solve it

--- A_D_Qs/Dec_06_decode_ways.cc
+++ A_D_Qs/Dec_06_decode_ways.cc
@@ -28,4 +28,25 @@ private:
 
 		return ans;
     }
+// version 2 // only DP
+ class Solution {
+public:
+    int numDecodings(string s)
+    {
+        if (s.length() == 0)
+            return 0;
+        int n = s.length();
+        vector<int> memo (n+1, 0); // memo [n]; total number of ways to make n
+        memo[0] = 1; // empty string
+        memo[1] = (s[0] != '0') ? 1 : 0; // string of size 1
+        for (int i = 2; i <= n; i++)
+        {
+            int first  = stoi(s.substr(i-1,1).c_str());
+            int second = stoi(s.substr(i-2,2).c_str());
+            memo[i] += (first >= 1 && first <= 9)? memo[i-1] : 0;
+            memo[i] += (second >= 10 && second <= 26)? memo[i-2] : 0;
+        }
+        return memo[n];
+    }
+};
 };
