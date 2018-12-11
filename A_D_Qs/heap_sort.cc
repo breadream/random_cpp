93d2cf773f6d8296b6f800c57b2c4c18d702e5d2
breadream <brad.dh.lim@gmail.com>
2018-12-11 01:23:47 -0600

    Added some comments regarding heap sort & complete binary tree:

--- A_D_Qs/heap_sort.cc
+++ A_D_Qs/heap_sort.cc
@@ -1,3 +1,10 @@
+// complete binary tree  (doesn't have to be a full binary tree)
+// - every level must be completely filled 
+// - all the leaf elements must lean towards the left 
+// - the last leaf element might not have a right sibling 
+// https://www.programiz.com/dsa/heap-sort
+// Heap Sort has O(nlogn) time complexities for all the cases ( best case, average case and worst case).
+
 #include <iostream>
 #include <vector>
 
@@ -8,9 +15,8 @@ using namespace std;
 void heapify(vector<int> &arr, int n, int i)
 {
 	int max = i; // initialize max as root 
-	cout << max << endl;
-	int left = 2*i + 1; 
-	int right = 2*i + 2;
+	int left = 2*i + 1; // left child
+	int right = 2*i + 2; // right child
 
 	// if left child is larger than root 
 	if (left < n && arr[left] > arr[max])
@@ -32,16 +38,26 @@ void heapify(vector<int> &arr, int n, int i)
 void heapSort(vector<int> &arr, int n)
 {
 	// Build heap 
+	// we start by heapifying the lowest smallest trees and 
+	// gradually move up until we reach the root element 
 	for (int i = n/2 -1; i >= 0; i--)
 		heapify(arr, n, i);
 	
+	/*
+	 * 1. Since the tree satisfies Max-Heap property, 
+	 * then the largest element is stored at the root node
+	 * 2. Remove the root element and put at the end of the array (nth position)
+	 * Put the last item of the tree (heap) at the vacant place.
+	 * 3. Reduce the size of the heap by 1 and heapify the root element again 
+	 * so that we have highest element at root.
+	 * 4. The process is repeated until all the items of the list is sorted. */
 	// Extract an element from heap, one by one 
 	for (int i = n-1; i >= 0; i--)
 	{
 		// Move current root to end 
-		swap(arr[i], arr[0]);
+		swap(arr[i], arr[0]); // step 2
 		// call max heapify on the reduced heap 
-		heapify(arr, i, 0);
+		heapify(arr, i, 0); // step 3
 	}	
 }
 
