147c7aac82ec3be022437529ed9574ebdd87f83f
breadream <brad.dh.lim@gmail.com>
2018-12-18 13:48:39 -0600

    Added trie data structure

---
+++ A_D_Qs/Dec_18_trie.cc
@@ -0,0 +1,41 @@
+struct Trie 
+{
+	bool finish; // mark the ending index
+	Trie* next[26];
+
+	Trie() : finish(false)
+	{
+		memseet(next, 0, sizeof(next));
+	}
+
+	~Trie() 
+	{
+		for (int i = 0; i < 26; i++)
+			if (next[i])
+				delete next[i];
+	}
+
+	void insert(const char* array)
+	{
+		if (*key == '\0')
+			finish = true; // mark the end of each string
+		else 
+		{
+			int curr = *key - 'A';
+			if (next[curr] == NULL)
+				next[curr] = new Trie(); // starting position? -> dynamically allocate 
+			next[curr]->insert(key+1); // insert next character
+		}
+	}
+
+	Trie* find (const char* key) 
+	{
+		if (*key == '\0') 
+			return this; // return the ending position of string 
+		int curr = *key - 'A';
+		if (next[curr] == NULL) // key not found
+			return NULL;
+		return next[curr]->find(key+1); // find next character 
+
+	}
+}
