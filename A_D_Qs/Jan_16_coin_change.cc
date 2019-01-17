b64ccafe188ccd212a5aab61b84fb7730bab17d6
breadream <brad.dh.lim@gmail.com>
2019-01-16 23:18:24 -0600

    Added coin change

--- A_D_Qs/Jan_16_coin_change.cc
+++ A_D_Qs/Jan_16_coin_change.cc
@@ -2,14 +2,16 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         // dp[i] = min coins to make up to amount i
-        vector<int> dp(amount + 1, INT_MAX);
+        vector<int> dp(amount + 1, amount+1);
         dp[0] = 0;
-        for (int coin : coins) 
-            for (int i = coin; i <= amount; ++i)
-                if (dp[i - coin] != INT_MAX)  
-                    dp[i] = min(dp[i], dp[i - coin] + 1);
+        // find the minimum number to get 1 to amount
+        for (int i = 1; i <= amount; i++)
+            for (int coin : coins) // try each coin
+                if (coin <= i) // coin should be less than or equal to amount
+                    dp[i] = min(dp[i], dp[i-coin]+1); // find more optimal solution, 
+                // dp[i-coin] -> previous subproblem, one is added since we use another coin
         
-        return dp[amount] == INT_MAX ? -1 : dp[amount];
-    }
+        // if there's no answer, dp[amount] is still -1 
+        return dp[amount] > amount ? -1 : dp[amount];
+ }                  
 };
-
