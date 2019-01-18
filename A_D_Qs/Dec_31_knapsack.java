213fd4cd90a55d8f5ad5b8622802db6fbf1e3a8f
breadream <brad.dh.lim@gmail.com>
2019-01-18 16:55:25 -0600

    Modified knapsack problem

--- A_D_Qs/Dec_31_knapsack.java
+++ A_D_Qs/Dec_31_knapsack.java
@@ -31,7 +31,7 @@ private int knapsack(Item[] items, int W, int i) {
 //only first 0 items can be included    0  [0   0   0   0   0   0] // we're including 0 items
 //only first 1 items can be included    1  [0   6   6   6   6   6] // we can include values[0] 
 //only first 2 items can be included    2  // either include second item(values[1]) or not include it // if we don't include the item, look above  [i-1][j]
-//only first 3 items can be included3	3
+//only first 3 items can be included	3
 
 //   	   	 0	 1	 2	 3	 4	 5
 //       0  [0   0   0   0   0   0]
@@ -47,12 +47,12 @@ public int knapsack(int W, int[] weights, int[] vals) {
 	int [][] cache = new int [weight.length+1][W+1];
 
 	for (int i = 1; i <= weight.length; i++) {
-		for (int j = 1; j <= W; j++) {
-			if (weights[i-1] > j)
-				cache[i][j] = cache[i-1][j];
+		for (int j = 1; j <= W; j++) { // j is the current max weight we're looking at 
+			if (weights[i-1] > j) // item is too big for the current max weight 
+				cache[i][j] = cache[i-1][j]; // no way to include the item, so refer to previous row cache value of same max weight 
 			else 
-				cache[i][j] = Math.max(cache[i-1][j],
-									   cache[i-1][j-weights[i-1]+vals[i-1]]);
+				cache[i][j] = Math.max(cache[i-1][j], // don't include the item, just get the previous item 
+									   cache[i-1][j - weights[i-1]] + vals[i-1]); // subtract the weight of current item to get the remaining max val
 		}
 	}
 }
