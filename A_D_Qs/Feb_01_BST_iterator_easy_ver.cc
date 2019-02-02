d986a3dafbda7c2f56d4be7babb7e86beb866d06
breadream <brad.dh.lim@gmail.com>
2019-02-01 16:46:05 -0800

    Added BST iterator easy version

---
+++ A_D_Qs/Feb_01_BST_iterator_easy_ver.cc
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class BSTIterator {
+public:
+    BSTIterator(TreeNode* root) {
+        iterator(root);
+    }
+    
+    void iterator(TreeNode* root)
+    {
+        if (!root)
+            return;
+        else 
+        {
+            pq.push(root->val);
+            iterator(root->left);
+            iterator(root->right);
+        }
+    }
+    /** @return the next smallest number */
+    int next() {
+        int min = pq.top();
+        pq.pop();
+        return min;
+    }
+    
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+        return !pq.empty();
+    }
+
+private:
+    priority_queue <int, vector<int>, greater<int>> pq;    
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
