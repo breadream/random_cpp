4bd12fad86b351ab738dcc0a92ec331f4f3ded72
breadream <brad.dh.lim@gmail.com>
2019-02-13 16:02:56 -0800

    Added reorder list

---
+++ A_D_Qs/Feb_13_reorder_list.cc
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+      if (!head || !head->next) 
+            return;
+        
+        deque<ListNode*> dq;
+        ListNode* ptr = head; // used for inserting nodes to deque
+        
+        while (ptr)
+        {
+            dq.push_back(ptr);
+            ptr = ptr->next;
+        }
+        // remove 1st head, because we start from real head node
+        dq.pop_front(); // if we don't remove it, head -> head ... 
+        
+        while (!dq.empty()) // interleave using read head ptr // in back and forth style  
+        {
+            head->next = dq.back(); 
+            dq.pop_back();
+            head = head->next;
+            if (!dq.empty())
+            {
+                head->next = dq.front();
+                dq.pop_front();
+                head = head->next;
+            }
+        }
+        head->next = nullptr; // mark the end as nullptr
+    }
+};
