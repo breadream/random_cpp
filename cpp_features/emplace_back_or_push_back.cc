c487ff5bbac8a1dfa7da0d95732d4ccc9ac449ee
breadream <brad.dh.lim@gmail.com>
2019-02-17 22:23:06 -0800

    Added more about push_back and emplace_back

--- cpp_features/emplace_back_or_push_back.cc
+++ cpp_features/emplace_back_or_push_back.cc
@@ -48,11 +48,22 @@ int main()
 	// the constructor and destructor are called twice 
 	// because push_back() needs an object of a class as a parameter
 	vecTest1.push_back(Test(1, "Test1")); 
+	/* String Opearation 
+	 * new string object implicitly created initialized with provided char* 
+	 * push_back is called and will copy this string into the vector using the move constructor
+	 * because the original string is a temporary object, then temporary object will be destroyed */
+
+
 	cout << "--------------------" << endl;
 	// emplace_back() finds the only constructor that can receive '2, "Test2"'
 	// it's more efficient because the constructor and destructor are called once
 	// because the compiler creates and add an object without copying or moving an object
 	vecTest2.emplace_back(2, "Test2"); 
+	/* String Operation
+	 * emplace_back(s) constructrs a string in-place w/o creating a temporary string 
+	 * emplace_back will be called directly with char* argument 
+	 * and then create a string to be stored in the vector initialized with this char* 
+	 * -> we can avoid constructing and destroying an unnecessary temporary string object */
 	
 	return EXIT_SUCCESS;
 }
