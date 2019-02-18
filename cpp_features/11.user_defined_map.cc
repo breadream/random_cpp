6de640528753c7c8b3a948890687545a7460d247
breadream <brad.dh.lim@gmail.com>
2019-02-18 15:28:29 -0800

    Added user defined map

--- cpp_features/11.user_defined_map.cc
+++ cpp_features/11.user_defined_map.cc
@@ -25,4 +25,33 @@ struct Key
 
 namespace std 
 {
+	template <> // explicit specialization of struct template hash
+	struct hash<Key> 
+	{
+		std::size_t operator() (const Key &k) const 
+		{
+			using std::size_t;
+			using std::hash;
+			using std::string;
+
+			// Compute individual hash values for first
+			// second and third and combine them using XOR
+			// and bit shifting 
+			return ((hash<string>()(k.first)
+					 ^ (hash<string>()(k.second) << 1)) >> 1 )
+					 ^ (hash<int>()(k.third) << 1);
+
+			// If you don't shift the bits and two strings were the same, 
+			// the XOR would cause them to cancel each other out. 
+			// e.g. hash("a","a",1) would be the same as hash("b","b",1) 
+		}
+	};
+}
+
+int main()
+{
+  std::unordered_map<Key,std::string> m6 = {
+    { {"John", "Doe", 12}, "example"},
+    { {"Mary", "Sue", 21}, "another"}
+  };
 }
