9147ec1a87ed2f0391b45f29a1b04071180931b2
breadream <brad.dh.lim@gmail.com>
2019-02-19 16:36:48 -0800

    Added how to create a user defined map

--- cpp_features/11.user_defined_map.cc
+++ cpp_features/11.user_defined_map.cc
@@ -55,3 +55,33 @@ int main()
     { {"Mary", "Sue", 21}, "another"}
   };
 }
+
+
+
+
+
+
+
+////////////////////////////////////////
+//
+struct KeyHasher
+{
+  std::size_t operator()(const Key& k) const
+  {
+    using std::size_t;
+    using std::hash;
+    using std::string;
+
+    return ((hash<string>()(k.first)
+             ^ (hash<string>()(k.second) << 1)) >> 1)
+             ^ (hash<int>()(k.third) << 1);
+  }
+};
+
+int main()
+{
+  std::unordered_map<Key,std::string,KeyHasher> m6 = {
+    { {"John", "Doe", 12}, "example"},
+    { {"Mary", "Sue", 21}, "another"}
+  };
+}
