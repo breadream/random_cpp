7c0c96f748d050fe500eaa239577a316c914d082
breadream <brad.dh.lim@gmail.com>
2019-03-04 22:32:16 -0800

    Added N queens template

---
+++ cpp_features/N_queens.cc
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<string>> solveNQueens(int n) {
+    }
+    
+private:
+    void solveNQueens(vector<vector<string>>& res, vector<string> &baseRow, int row, int &colNum)
+    {
+    }
+    
+    bool isValid(vector<string>& baseRow, int row, int col, int &colNum)
+    {
+        // check if there's a queen in the same column
+        
+        //  north-west diagonal 
+        
+        // north-east diagonal 
+        
+        return true;
+    }
+};
