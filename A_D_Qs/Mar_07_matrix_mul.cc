d229637d5c17198ea707d63d1a7fc1ded71b21d7
breadream <brad.dh.lim@gmail.com>
2019-03-08 13:17:35 -0800

    Added matrix multiplication

---
+++ A_D_Qs/Mar_07_matrix_mul.cc
@@ -0,0 +1,9 @@
+   public int[][] productMatrix(int[][] A, int[][] B) {
+        int[][] answer = new int[A.length][B[0].length];
+        for(int i=0; i<answer.length;i++){
+            for(int j=0; j<answer[0].length;j++){
+               for(int k=0; k<A[0].length;k++){
+                    answer[i][j] += A[i][k]*B[k][j];
+                }
+            }
+        }
