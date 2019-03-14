ed16d63fe7892259393288587c8e11bb014b217a
breadream <brad.dh.lim@gmail.com>
2019-03-13 20:30:09 -0700

    Added level order traversal using hash map

--- A_D_Qs/Mar_13_level_order_traversal.cc
+++ A_D_Qs/Mar_13_level_order_traversal.cc
@@ -5,55 +5,99 @@ struct Node
 	Node* right;
 };
 
-bool printEachLevel(Node* root, int level)
+namespace Recursive 
 {
-	if (!root)
-		return false;
-
-	if (level == 1)
+	/* O(N^2) (worst case), because # of levels in a binary tree could be equal to 
+	 * number of nodes for skewed trees */
+	bool printEachLevel(Node* root, int level)
 	{
-		cout << root->val << endl;
-		return true;
+		if (!root)
+			return false;
+
+		if (level == 1)
+		{
+			cout << root->val << endl;
+			return true;
+		}
+
+		// go down to their subtree as much as 'level'
+		bool left  = printEachLevel(root->left, level-1);
+		bool right = printEachLevel(root->right, level-1);
+		
+		return left || right;
 	}
 
-	// go down to their subtree as much as 'level'
-	bool left  = printEachLevel(root->left, level-1);
-	bool right = printEachLevel(root->right, level-1);
-	
-	return left || right;
-}
-
-bool printEachLevel (Node* root)
-{
-	if (!root)
-		return false;
-
-	Node* curr = nullptr;
-	queue<Node*> queue;
-	queue.push(root);
-
-	while (!queue.empty())
+	void levelOrderTraversal(Node* root)
 	{
-		curr = queue.front();	
-		pop_front();
+		if (!root)
+			return;
 
-		cout << curr->val << out;
+		int level = 1;
 
-		if (curr->val)
-			queue.push(curr->left);
-
-		if (curr->right)
-			queue.push(curr->right);
+		while (printEachLevel(root, level))
+			level++;
 	}
 }
 
-void levelOrderTraversal(Node* root)
+namespace Iterative 
 {
-	if (!root)
-		return;
+	/* O(N), with extra space 
+	 * it requires space proportional to the maximum number of nodes at given depth */
+	bool printEachLevel (Node* root)
+	{
+		if (!root)
+			return false;
 
-	int level = 1;
+		Node* curr = nullptr;
+		queue<Node*> queue;
+		queue.push(root);
 
-	while (printEachLevel(root, level))
-		level++;
+		while (!queue.empty())
+		{
+			curr = queue.front();	
+			pop_front();
+
+			cout << curr->val << out;
+
+			if (curr->val)
+				queue.push(curr->left);
+
+			if (curr->right)
+				queue.push(curr->right);
+		}
+	}
 }
+
+namespace UsingHashMap 
+{
+	void preorder(Node* root, int level, auto& map)
+	{
+		if (!root)
+			return;
+
+		map[level].emplace_back(root->val);
+
+		// recursively traverse its left subtree by increasing level by 1 
+		preorder(root->left, level+1, map);
+		preorder(root->right, level+1, map);
+	}
+	// solve the problem in preorder fashion
+	void levelOrderTraversal (Node* root)
+	{
+		if (!root)
+			return;
+
+		map <int, vector<int>> map;
+
+
+		for (const auto & it : map)
+		{
+			cout << "Level " << it.first << ": ";
+			for (const int i : it.second)
+				cout << i  << " ";
+			cout << endl;
+		}
+	}
+
+}
+
