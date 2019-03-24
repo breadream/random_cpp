78326d640865fa8ae2d158c7654c2e0b58fc78bd
breadream <brad.dh.lim@gmail.com>
2019-03-23 17:57:06 -0700

    Added copy constructor deep copy and shallow copy

---
+++ cpp_features/simple_hash_function.cc
@@ -0,0 +1,17 @@
+// The reason why prime numbers are used is to minimize collisions 
+// when the data exhibits some particular patterns
+#define A 54059 /* a prime */
+#define B 76963 /* another prime */
+#define C 86969 /* yet another prime */
+#define FIRSTH 37 /* also prime */
+
+size_t hashStr(const char* s)
+{
+   size_t h = FIRSTH;
+   while (*s) 
+   {
+     h = (h * A) ^ (s[0] * B);
+     s++;
+   }
+   return h; // or return h % C;
+}
