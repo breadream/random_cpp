95c6dd2212e38de6a3ff7ac8b9915c82d7526f54
breadream <brad.dh.lim@gmail.com>
2019-03-24 21:46:04 -0700

    Modified size_t stuff

--- cpp_features/14.size_t_explanation.cc
+++ cpp_features/14.size_t_explanation.cc
@@ -1,7 +1,7 @@
-// size_t is an unsigned integer type that is the result of 
-// the "sizeof" operator
+// size_t is an unsigned integer type that is the result of the "sizeof" operator
 // we can be certain that it is big enough to contain the size of 
 // the biggest object out system can handle 
+
 // size_t is never negative. Since it is unsigned,
 // we can store numbers that are roughly twice as big as in the 
 // corresponding signed type 
