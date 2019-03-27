559d94c9b90e4fd9ce61ce5c0d240363ed719753
breadream <brad.dh.lim@gmail.com>
2019-03-27 16:50:37 -0700

    Added delete k palindrome

---
+++ A_D_Qs/Mar_27_delete_k_palindrome.cc
@@ -0,0 +1,16 @@
+// waterrfetawx
+// scanning over the first and the last element and if they are not the same,
+// then decrement the number of k 
+// waterrfetawx
+
+bool palindrome_check (const string &s, int k)
+{
+	string copy(s);
+	int i = 0, j = s.size()-1;
+	while (i <= j)
+	{
+		if (s[i] != s[j])
+
+	}
+}
+
