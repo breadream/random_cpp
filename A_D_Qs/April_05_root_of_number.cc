b7940fc1560eaa8523f7304cc29145d515580ea0
breadream <brad.dh.lim@gmail.com>
2019-04-05 15:31:17 -0700

    Added root of number again

--- A_D_Qs/April_05_root_of_number.cc
+++ A_D_Qs/April_05_root_of_number.cc
@@ -1,25 +1,20 @@
-// x ; non-negative, n = positive 
 double root(double x, unsigned int n) 
 {  
-  if (x == 0)
-    return 0;
+    double lowerLimit  = 0;
+    double higherLimit = (x > 1) ? x : 1;
+    double tempRoot = (lowerLimit + higherLimit) / 2;
   
-  double lowerBound = 0;
-  double upperBound = (x > 1) ? x : 1; // in case if it is less than 1 
-  double approxRoot = (lowerBound + upperBound) / 2; 
-  // 0     9 
-  // 4.5 ^ 
-  while (approxRoot - lowerBound >= 0.001) 
-  {
-    if (pow(approxRoot, n) > x)
-      upperBound = approxRoot;
-    else if (pow(approxRoot, n) < x)
-      lowerBound = approxRoot;
-    else
-      break;
-    
-    approxRoot = (upperBound + lowerBound) / 2;
-  }
-  return approxRoot;
+    while (tempRoot - lowerLimit >= 0.001)
+    {
+       if (pow(tempRoot, n) > x)
+         higherLimit = tempRoot;
+       else if (pow(tempRoot, n) < x)
+         lowerLimit = tempRoot;
+       else
+         break;
+       tempRoot = (lowerLimit + higherLimit) / 2;
+    }
+  
+  return tempRoot;   
 }
 
