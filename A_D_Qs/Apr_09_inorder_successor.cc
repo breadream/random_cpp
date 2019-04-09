c8c14955a52bfea76e9eef3feea1c6e39a569432
breadream <brad.dh.lim@gmail.com>
2019-04-09 15:39:01 -0700

    Added inorder successor

---
+++ A_D_Qs/Apr_09_inorder_successor.cc
@@ -0,0 +1,53 @@
+/* If you store parent link, 
+ * 1. If the current node has a right child, the leftmost child of the right child is the inorder successor
+ *    if the current node has no right child, the right child is the inorder successor 
+ * 2. Navigate up the parent ancestor nodes, 
+ * 	  and when you find a parent whose left child is the node you're currently at, 
+ * 	  the parent is the inorder successor of your original node.
+ * 	  */
+
+TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+	// Case 1 : Right subtree is not empty
+	if (p->right)
+	{
+		TreeNode* curr = p->right;
+		// loop down to find the leftmost leaf
+		while(curr->left)
+			curr = curr->left;
+
+		return curr;
+	}
+
+	// Case 2 : Right subtree is empty
+	// Start from root and search for successor down the tree
+	TreeNode* succ = nullptr;
+	while (root)
+	{
+		if (p->val < root->val)
+		{
+			succ = root;
+			root = root->left;
+		}
+		else if (p->val > root->val)
+			root = root->right;
+		else if (p->val == root->val)
+			break;
+	}
+	return succ;
+}
+
+TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) 
+{
+	TreeNode* succ = nullptr;
+	while (root)
+	{
+		if (p->val < root->val)
+		{
+			succ = root;
+			root = root->left;
+		}
+		else if (p->val >= root->val)
+			root = root->right;
+	}
+	return succ;
+}
