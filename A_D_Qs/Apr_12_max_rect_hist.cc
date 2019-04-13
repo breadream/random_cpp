0924ebb085157100d79fed0e63f9c5fd937cb515
breadream <brad.dh.lim@gmail.com>
2019-04-12 17:10:38 -0700

    Added maximum rectangle in histogram

--- A_D_Qs/Apr_12_max_rect_hist.cc
+++ A_D_Qs/Apr_12_max_rect_hist.cc
@@ -1,34 +1,32 @@
- int largestRectangleArea(vector<int>& heights) {
+i int largestRectangleArea(vector<int>& heights) {
         stack<int> stack; // to store each bar's index
         stack.push(-1); // to mark the end
         int maxArea = 0;
         
         for (int i = 0; i < heights.size(); i++)
         {
-            // keep pushing the current bar's index onto the stack 
-            // until we get two successive numbers in descending order 
-            // heights[stack.top()] -> previous bar  
-            while (stack.top() != -1 && heights[stack.top()] >= heights[i])
+            // if we found two successive numbers in desecnding order
+            while (stack.top() != -1 && heights[stack.top()] >= heights[i])  
             {
                 int topVal = stack.top();
                 stack.pop();
-                // find out the area of rectangle formed using the current element 
-                // as the height of the rectangle 
-                // and the difference between the current element's index 
-                // and the element stack[top-1]-1 as the width 
-                maxArea = std::max(maxArea, heights[topVal] * (i-stack.top()-1));
-                // heights[topVal] = height
-                // i - stack[top-1] -1 = width
+                // heights[topVal] = height of the rectangle
+                // (i - stack.top()-1) = width of the rectangle 
+                // (i; anchor point, stack.top()+1; left end of the rectangle)
+                maxArea = std::max(maxArea, heights[topVal] * (i-(stack.top()+1)));
             }
+            // keep pushing the current bar's index onto the stack 
+            // until we get two successive numbers in descending order 
             stack.push(i);
         }
         
+        // process the indices still in the stack
         while (stack.top() != -1)
         {
             int len = heights.size();
             int topVal = stack.top();
             stack.pop();
-            maxArea = std::max(maxArea, heights[topVal] * (len-stack.top()-1));
+            maxArea = std::max(maxArea, heights[topVal] * (len-(stack.top()+1)));
         }
         return maxArea;     
     }
